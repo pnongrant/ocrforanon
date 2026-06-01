1a2163fca41b46f30c472062f3ab55b05271dadb
pnongrant <pnongrant@gmail.com>
2026-06-01 07:32:52 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,2 @@
-# Токен вашего Telegram бота (получить у @BotFather)
-BOT_TOKEN = "8994510685:AAG8vb24SibDHwI46YQUop7I7xuGjB_U6fs"
-
-# API ключ Google Vision или используем Tesseract (локально)
-# Для лучшего качества рекомендую Google Vision API
-USE_GOOGLE_VISION = True  # True = Google Vision, False = Tesseract + OpenCV
-
-GOOGLE_VISION_API_KEY = "AIzaSyDBIe9JufZGFPcQx-cjiVzJeTMay7tWUXU"  # если USE_GOOGLE_VISION = True
+BOT_TOKEN = "8895102497:AAElE-0_d4gak3Q9BxQwUmXy1-4mOoFanL0"
+GOOGLE_VISION_API_KEY = "AIzaSyDBk9m4bcZq5ludyZ7Q29EbTzOaHZQMhQE"
